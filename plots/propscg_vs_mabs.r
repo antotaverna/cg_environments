9e944f04e715cb6e62676d06000a3ffa60480010
antotaverna <ataverna@unc.edu.ar>
2022-03-06 23:49:28 -0300

    update scatter mag vs props c/quartiles

--- plots/propscg_vs_mabs.r
+++ plots/propscg_vs_mabs.r
@@ -4,6 +4,7 @@
 #source("propscg_vs_mabs.r")
 library(bootstrap); library(latex2exp); library(astrolibR); library(magicaxis)
 library(Hmisc)
+library(fabricatr)
 #
 #
 #--------------------------------------------------------------
@@ -15,179 +16,98 @@ HH<-read.table("../data/tab_gal_gru.dat"); VG<-read.table("../data/compact_in_vo
 
 
 colnames(SS)[c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)] <- c('igru','nmi','alcm','delcm','zmedian','radio_mins','mu','sigv','rmag_bri','flag','rabs1','dij','tcr','rp','rabs2')
-                                                                                                                                                                          
 colnames(Gf)[c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)] <- c("igru",'nmi','alcm','delcm','zmedian','radio_mins','mu','sigv','rmag_bri','flag','rabs1','dij','tcr','rp','rabs2')
-                                                                                                                                                                          
 colnames(S)[c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)] <-  c("igru",'nmi','alcm','delcm','zmedian','radio_mins','mu','sigv','rmag_bri','flag','rabs1','dij','tcr','rp','rabs2')
-                                                                                                                                                                          
 colnames(FF)[c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)] <- c("igru",'nmi','alcm','delcm','zmedian','radio_mins','mu','sigv','rmag_bri','flag','rabs1','dij','tcr','rp','rabs2')
-                                                                                                                                                                          
 colnames(VG)[c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17)] <- c("igru",'nmi','alcm','delcm','zmedian','radio_mins','mu','sigv','rmag_bri','flag','rabs1','dij','tcr','rp','rabs2','tipo','estado')
 
 colnames(HH)[c(1,2,3,4,5,6,7)] <-c("GId", "Nm", "RA", "Dec", "Redshift", "mag_r", "mag_g")
 
+#------------------------------------------------------------------------------------------------------------
+
+
+
 #--------
 bin2=3
 #--------
-laby=TeX('$\\sigma$') 
-labx=TeX('$M_{bri} - 5log_{10}h$')
-
-#pdf("scatter_mags_eq_bins.pdf")
-#pdf("scatter_mags_eq_bins_2.pdf")
-
-par(family="serif")
-par(cex.lab=1.1)       #Tamaño labels
-par(cex.axis=0.8)      #Tamaño números en ejes.
-par(lwd=1)
-par(cex=1)
-par(mgp=c(1.,0.2,0))   #margen labels
-par(mar=c(0,3,0,1))  #c(b,l,t,r)
-par(oma=c(6,1,5,1))  #c(b,l,t,r)
-#par(mfrow=c(3,3))
-par(mfcol=c(3,3))
-
-R_no<-S$rabs1 ;  N_no=length(R_no)
-R_gg<-SS$rabs1 ; N_gg=length(R_gg)
-R_fi<-FF$rabs1 ; N_fi=length(R_fi)
-R_cp<-Gf$rabs1 ; N_cp=length(R_cp)
-R_vv<-VG$rabs1;  N_vv=length(R_vv)
-
-R_vr=subset(VG$rabs1,VG$tipo==0)
-R_vs=subset(VG$rabs1,VG$tipo==1)
-N_vr=length(subset(VG$rabs1,VG$tipo==0))
-N_vs=length(subset(VG$rabs1,VG$tipo==1))
-
-sig_no<-S$sigv   ;rp_no<-S$rp   ;tcr_no<-S$tcr  
-sig_gg<-SS$sigv  ;rp_gg<-SS$rp  ;tcr_gg<-SS$tcr
-sig_fi<-FF$sigv  ;rp_fi<-FF$rp  ;tcr_fi<-FF$tcr
-sig_cp<-Gf$sigv  ;rp_cp<-Gf$rp  ;tcr_cp<-Gf$tcr
-sig_vv<-VG$sigv  ;rp_vv<-VG$rp  ;tcr_vv<-VG$tcr
-
-sig_vr=subset(VG$sigv,VG$tipo==0)
-sig_vs=subset(VG$sigv,VG$tipo==1)
-rp_vr=subset(VG$rp,VG$tipo==0)
-rp_vs=subset(VG$rp,VG$tipo==1)
-tcr_vr=subset(VG$tcr,VG$tipo==0)
-tcr_vs=subset(VG$tcr,VG$tipo==1)
-
-
-##################################################
-##################################################
-##################################################
-#### VOIDS S y R
-##################################################
-##################################################
-
-
-#--------
-#------------------------------------------------------------------------------------------------------------
-# ------ CG in filaments-------------------------------------------------------------------------------------
-R_no<-S$rabs1
-R_gg<-SS$rabs1
-R_fi<-FF$rabs1
-R_cp<-Gf$rabs1
-R_vv<-VG$rabs1
-
-a=c(R_no,R_gg,R_fi,R_cp,R_vv)
-amax=max(a)
-amin=min(a)
-
+################################################################
 result <- data.frame()
 
 func_fil <- function(arg1,arg2,arg3){
 	# arg1 = Mr o deltaM
 	# arg2 = props: sigma, rproy, tcr
 	# arg3 = length Mr
-#------------------PLOT
-min_f=min(arg1); max_f=max(arg1)+0.1;   
-#min_f=amin; max_f=amax+0.1;   
-dM=(max_f-min_f)/(bin2)
-a = seq(0.0,1,0.33333)
-quantile(R_fi, a, na.rm = TRUE)
+    
+    q=split_quantile(x=arg1,type=3)
+    size_q1=length(arg1[q==1])
+    size_q2=length(arg1[q==2])
+    size_q3=length(arg1[q==3])
+    
+    for(i in 1:3){
+    xmed[i]=mean(arg1[q==i])
+    ymed[i]=mean(arg2[q==i])
+    }
 
 
+	#------------------BOOTSTRAP
+	error_f<-vector("logical",bin2)
+	boot=250; ff=0; jj=0
+    count_ff<-vector(mode="logical",boot) 
+    
+    for(k in 1:3){
+    
+        r<-arg2[q==k]
+        theta <- function(r){r}  ;  results<-bootstrap(r,boot,theta)
+    
+        size_q=length(arg1[q==k])
+    
+        for(i in 1:boot){
+	      for(hh in 1:size_q){
+	          try({jj=jj+results$thetastar[hh,i]}, silent=TRUE)}
+	      
+             count_ff[i]<-jj/size_q
+             ff=0; jj=0
+          }
+          error_f[k]<-sd(count_ff)
+        }
 
-library(classInt)
-
-x <- classIntervals(dataset, 3, style = 'quantile')
-x$var
-x$brks
-
-new_vec <- cut(R_fi , breaks = quantile(R_fi, a, na.rm = TRUE), include.lowest=TRUE)
-
-error_f<-vector("logical",bin2) ;  prop_f<-vector('logical',bin2)
-error2_f<-vector("logical",bin2);  count_f<-vector('logical',bin2)
-colmed_f<-vector("logical",bin2);  FL_f<-vector("logical",bin2)
-a_f<-vector("logical",arg3)
-
-count_f[1:bin2]=0;#lo agregue yo 
-prop_f[1:bin2]=0
-for(i in 1:arg3){
-    ibin=as.integer((arg1[i]-min_f)/dM)+1
-    if(ibin<1)   {ibin=1}
-    if(ibin>bin2){ibin=bin2}
-    k=ibin
-    count_f[k]=count_f[k]+1
-    prop_f[k]=prop_f[k]+arg2[i]
-    a_f[i]<- arg2[i]
-   }
-  
-for(k in 1:bin2){
-   colmed_f[k]=min_f+(dM/2.)*(2*k-1)
-   FL_f[k]= (prop_f[k]/count_f[k]) #normalizado Nbin
-   error2_f[k]=dM
-  }
-
-#------------------BOOTSTRAP
-boot=250; ff=0; jj=0
-count_ff<-vector(mode="logical",boot) 
-for(k in 1:bin2){
-    r<-a_f[1:count_f[k]] #;  rr<-aa_f[1:new_count_f[k],k]
-    theta <- function(r){r}  ;  results<-bootstrap(r,boot,theta)
-#   theta <- function(rr){rr};results2<-bootstrap(rr,boot,theta)
-    for(i in 1:boot){
-      for(hh in 1:count_f[k]){
-          try({jj=jj+results$thetastar[hh,i]}, silent=TRUE)
-      }
-      
-      count_ff[i]<-jj/count_f[k] 
-
-     ff=0; jj=0
-     }
-     error_f[k]<-sd(count_ff)
-   }
-  for(k in 1:bin2){
-  if(error_f[k] == 0){error_f[k] = 0.02}
-  }
-#------------------
-
-
-  result=data.frame(colmed_f,FL_f,error_f)
-  return(result)
+        for(k in 1:bin2){
+           if(error_f[k] == 0){error_f[k] = 0.02}
+        }
+    
+        result=data.frame(xmed,ymed,error_f)
+        return(result)
 } #fin function
+################################################################
 
 
 #-------------------------------------------
 #-------------------------------------------
+col2alpha <- function(col, alpha) {
+  col_rgb <- col2rgb(col)/255
+  rgb(col_rgb[1], col_rgb[2], col_rgb[3], alpha = alpha)
+}
 
-
+#-------------------------------------------
+#-------------------------------------------
 func_plot <- function(xx,yy,cc){
 	# arg1 = x
 	# arg2 = y
 	# arg3 = lim y
 	# arg4 = color
-#------------------PLOT
-points(xx,yy,type='l',col=cc,main='',lwd=2,asp=-5,xaxt='n')#,yaxt='n')
-points(xx,yy,pch=16,type='p',col=cc,main='',lwd=2,asp=-5,xaxt='n',yaxt='n',cex=1)
-#polygon(c(xx,rev(xx)),c(yy+erry*0.5,rev(yy-erry*0.5)),col=rgb(1, 101/255, 0,0.5),border=NA)
+    #------------------PLOT
+    points(xx,yy,type='l',col=cc,main='',lwd=2,asp=-5,xaxt='n')#,yaxt='n')
+    points(xx,yy,pch=16,type='p',col=cc,main='',lwd=2,asp=-5,xaxt='n',yaxt='n',cex=1)
+    polygon(c(xx,rev(xx)),c(yy+erry,rev(yy-erry)),col=col2alpha(cc,0.3),border=NA)#
+    #polygon(c(xx,rev(xx)),c(yy+erry*0.5,rev(yy-erry*0.5)),col=col2alpha(cc,0.3),border=NA)#
 } #fin function
+
 #-------------------------------------------
 #-------------------------------------------
 laby=TeX('$\\sigma$') 
 labx=TeX('$M_{bri} - 5log_{10}h$')
 
-#pdf("scatter_mags_eq_bins_vRyS.pdf")
-#pdf("scatter_mags_eq_bins_2_vRyS.pdf")
+#cairo_pdf("scatter_mags_quartiles.pdf")
 
 par(family="serif")
 par(cex.lab=1.1)       #Tamaño labels
@@ -228,44 +148,38 @@ tcr_vs=subset(VG$tcr,VG$tipo==1)
 ###################################################################
 ######################### Mr vs Sigma #############################
 ###################################################################
- 
+limxmag=c(-23.0,-20.0)
 
-return_fil=func_fil(R_fi,sig_fi,N_fi) #colmed_f,FL_f,error_f
+return_fil=func_fil(R_fi,sig_fi,N_fi)
 xx <- return_fil[[1]]; yy <- return_fil[[2]]; erry <- return_fil[[3]]
 laby=TeX('$\\sigma$ \\[km s$^{-1}$\\]')
 plot(xx,yy,ylim=c(100,600),xlim=limxmag,xlab=c(100,600),ylab=laby,xaxt='n',type="n")#,yaxt='n')
 func_plot(xx,yy,"orange")
-polygon(c(xx,rev(xx)),c(yy+erry*0.5,rev(yy-erry*0.5)),col=rgb(1, 0, 0,0.3),border=NA)
 
 #----------
-return_gg=func_fil(R_gg,sig_gg,N_gg) #colmed_f,FL_f,error_f
+return_gg=func_fil(R_gg,sig_gg,N_gg)
 xx <- return_gg[[1]]; yy <- return_gg[[2]]; erry <- return_gg[[3]]
 func_plot(xx,yy,'magenta')
-polygon(c(xx,rev(xx)),c(yy+erry*0.5,rev(yy-erry*0.5)),col=rgb(1,0,0,0.3),border=NA)
 
 #----------
-return_no=func_fil(R_no,sig_no,N_no) #colmed_f,FL_f,error_f
+return_no=func_fil(R_no,sig_no,N_no)
 xx <- return_no[[1]]; yy <- return_no[[2]]; erry <- return_no[[3]]
 func_plot(xx,yy,'red')
-polygon(c(xx,rev(xx)),c(yy+erry*0.5,rev(yy-erry*0.5)),col=rgb(1, 101/255, 0,0.5),border=NA)
 
 #----------
-return_cp=func_fil(R_cp,sig_cp,N_cp) #colmed_f,FL_f,error_f
+return_cp=func_fil(R_cp,sig_cp,N_cp)
 xx <- return_cp[[1]]; yy <- return_cp[[2]]; erry <- return_cp[[3]]
 func_plot(xx,yy,'darkblue')
-polygon(c(xx,rev(xx)),c(yy+erry*0.5,rev(yy-erry*0.5)),col=rgb(0, 0, 1,0.3),border=NA)
 
 #----------
-return_vs=func_fil(R_vs,sig_vs,N_vs) #colmed_f,FL_f,error_f
+return_vs=func_fil(R_vs,sig_vs,N_vs)
 xx <- return_vs[[1]]; yy <- return_vs[[2]]; erry <- return_vs[[3]]
 func_plot(xx,yy,'darkgreen')
-polygon(c(xx,rev(xx)),c(yy+erry*0.5,rev(yy-erry*0.5)),col=rgb(0, 1, 0,0.3),border=NA)
 
 #----------
-return_vr=func_fil(R_vr,sig_vr,N_vr) #colmed_f,FL_f,error_f
+return_vr=func_fil(R_vr,sig_vr,N_vr)
 xx <- return_vr[[1]]; yy <- return_vr[[2]]; erry <- return_vr[[3]]
 func_plot(xx,yy,'Deepskyblue3')
-polygon(c(xx,rev(xx)),c(yy+erry*0.5,rev(yy-erry*0.5)),col=rgb(1, 1, 0,0.3),border=NA)
 
 
 legend(-23.,590,c("Node", "Filaments",'Loose'),bty="n",lty=c(1,1,1,1,1,1), col=c('red',"orange","magenta"),horiz=FALSE,inset=0,cex=0.9,pch=c(16,16,16,16,16))
@@ -273,10 +187,10 @@ legend(-21.65,590,c('Non-Embedded','Voids-S','Voids-R'),bty="n",lty=c(1,1,1,1,1,
 
 text(-22.5,150,label='A',cex=1.2)
 
-magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
 
 ###################################################################
 ######################### Mr vs Rproy #############################
@@ -316,10 +230,10 @@ func_plot(xx,yy,'Deepskyblue3')
 
 text(-22.5,30,label='B',cex=1.2)
 
-magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
 
 
 ###################################################################
@@ -362,10 +276,10 @@ func_plot(xx,yy,'Deepskyblue3')
 text(-22.5,0.01,label='C',cex=1.2)
 mtext(TeX('$M_{bri}-5log_{10}(h)$'), side = 1, cex = 0.8, line = 2.2, col = "black")
 
-magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
 
 
 ###################################################################
@@ -413,10 +327,10 @@ func_plot(xx,yy,'Deepskyblue3')
 
 legend(-20.,134,c("Node", "Filaments",'Groups','Field','Voids'),bty="n",lty=c(1,1,1,1,1), col=c('red',"orange","magenta",'darkblue','black'),horiz=FALSE,inset=0,cex=0.5,pch=c(16,18,17,20,21))
 
-magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
 
 ###################################################################
 ######################### Mr vs dij ################################
@@ -465,10 +379,10 @@ func_plot(xx,yy,'Deepskyblue3')
 
 legend(-20.,134,c("Node", "Filaments",'Groups','Field','Voids'),bty="n",lty=c(1,1,1,1,1), col=c('red',"orange","magenta",'darkblue','black'),horiz=FALSE,inset=0,cex=0.5,pch=c(16,18,17,20,21))
 
-magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
 
 ###################################################################
 ######################### Mr vs tita ################################
@@ -518,10 +432,10 @@ mtext(TeX('$M_{bri}-5log_{10}(h)$'), side = 1, cex = 0.8, line = 2.2, col = "bla
 
 legend(-20.,134,c("Node", "Filaments",'Groups','Field','Voids'),bty="n",lty=c(1,1,1,1,1), col=c('red',"orange","magenta",'darkblue','black'),horiz=FALSE,inset=0,cex=0.5,pch=c(16,18,17,20,21))
 
-magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
 
 ###################################################################
 ######################### DeltaM12 vs rp ##########################
@@ -540,50 +454,8 @@ a=c(delta12_no,delta12_gg,delta12_fi,delta12_cp,delta12_vv)
 amax=max(a)
 amin=min(a)
 
-result <- data.frame()
 
-func_fil <- function(arg1,arg2,arg3){
-	# arg1 = Mr o deltaM
-	# arg2 = props: sigma, rproy, tcr
-	# arg3 = length Mr
-#------------------PLOT
-#min_f=min(arg1); max_f=max(arg1)+0.1;   
-#dM=(max_f-min_f)/(bin2)
-min_f=amin; max_f=amax+0.1;   
-dM=(max_f-min_f)/(bin2)
-
-error_f<-vector("logical",bin2) ;  prop_f<-vector('logical',bin2)
-error2_f<-vector("logical",bin2);  count_f<-vector('logical',bin2)
-colmed_f<-vector("logical",bin2);  FL_f<-vector("logical",bin2)
-a_f<-vector("logical",arg3)
-
-count_f[1:bin2]=0;#lo agregue yo 
-prop_f[1:bin2]=0
-for(i in 1:arg3){
-    ibin=as.integer((arg1[i]-min_f)/dM)+1
-    if(ibin<1)   {ibin=1}
-    if(ibin>bin2){ibin=bin2}
-    k=ibin
-    count_f[k]=count_f[k]+1
-    prop_f[k]=prop_f[k]+arg2[i]
-    a_f[i]<- arg2[i]
-   }
-  
-for(k in 1:bin2){
-   colmed_f[k]=min_f+(dM/2.)*(2*k-1)
-   FL_f[k]= (prop_f[k]/count_f[k]) #normalizado Nbin
-   error2_f[k]=dM
-  }
-
-#------------------
-
-  result=data.frame(colmed_f,FL_f,error_f)
-  return(result)
-} #fin function
-
-
-
-xx_no<-delta12_no  ;yy_no<-S$rp 
+xx_no<-delta12_no ;yy_no<-S$rp 
 xx_gg<-delta12_gg ;yy_gg<-SS$rp 
 xx_fi<-delta12_fi ;yy_fi<-FF$rp
 xx_cp<-delta12_cp ;yy_cp<-Gf$rp 
@@ -623,10 +495,10 @@ xx <- return_vr[[1]]; yy <- return_vr[[2]]; erry <- return_vr[[3]]
 func_plot(xx,yy,'Deepskyblue3')
 
 
-magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
 
 ###################################################################
 ######################### DeltaM12 vs sigma #######################
@@ -674,10 +546,10 @@ func_plot(xx,yy,'Deepskyblue3')
 
 
 
-magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
 
 ###################################################################
 ######################### DeltaM12 vs sigma #######################
@@ -728,10 +600,10 @@ func_plot(xx,yy,'Deepskyblue3')
 mtext(expression(paste(M[2],"-",M[1])), side=1, line=2, cex=0.8)
 #mtext(expression(Delta~price), side = 1, cex = 1, line = 2.2, col = "black")
 
-magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
-magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(1,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(2,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(3,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
+#magaxis(4,majorn=5, minorn=5, tcl=0.3, ratio=0.5,labels=FALSE)
 
 #dev.off()
 
